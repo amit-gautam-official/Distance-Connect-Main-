a9e8c76f41fe85e1681c17e2a994435684aa9d36
Talha Ansari <talhaansari1606@gmail.com>
2025-04-02 17:45:48 +0530

    build fix

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,9 +1,17 @@
 import { createClient } from 'contentful';
 
 import { env } from "@/env";
+
+// Make sure we have the required environment variables
+if (!process.env.CF_SPACE_ID || !process.env.CF_DELIVERY_ACCESS_TOKEN) {
+  console.error('Missing Contentful configuration! Please check your environment variables:');
+  console.error('- CF_SPACE_ID: ' + (env.CF_SPACE_ID ? 'Present' : 'Missing'));
+  console.error('- CF_DELIVERY_ACCESS_TOKEN: ' + (env.CF_DELIVERY_ACCESS_TOKEN ? 'Present' : 'Missing'));
+}
+
 const client = createClient({
-    space: env.CF_SPACE_ID!,
-    accessToken: env.CF_DELIVERY_ACCESS_TOKEN! 
-  });
+  space: env.CF_SPACE_ID!,
+  accessToken: env.CF_DELIVERY_ACCESS_TOKEN!,
+});
 
 export default client;
